6448edbb0c98fbab16dca08de76e79682eec16b5
Sai Harshith <saiharshith987@gmail.com>
2024-02-10 14:28:25 +0530

    first commit

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import Canvas from "./canvas";
+import { Customizer, Home } from "./pages";
+
+function App() {
+  return (
+    <main className="app transition-all ease-in">
+      <Home />
+      <Canvas />
+      <Customizer />
+    </main>
+  );
+}
+
+export default App;
